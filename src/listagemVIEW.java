00a6ae3cff409237afd8614fb7d94911a1642a8d
Stephanie <stephaniefelixx@hotmail.com>
2024-11-10 22:25:44 -0300

    Adicionada a funcionalidade do botao consultar vendas na tela de listagem

--- src/listagemVIEW.java
+++ src/listagemVIEW.java
@@ -148,8 +148,8 @@ public class listagemVIEW extends javax.swing.JFrame {
     }//GEN-LAST:event_btnVenderActionPerformed
 
     private void btnVendasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVendasActionPerformed
-        //vendasVIEW vendas = new vendasVIEW(); 
-        //vendas.setVisible(true);
+  VendasVIEW vendasView = new VendasVIEW();
+    vendasView.setVisible(true);    
     }//GEN-LAST:event_btnVendasActionPerformed
 
     private void btnVoltarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVoltarActionPerformed
